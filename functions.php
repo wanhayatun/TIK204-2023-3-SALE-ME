8764a767fb2ea3b0d655d24f57aa0a7c6debeb11
Wan Hayatun Nisa <wan_hayatu@mhs.unsyiah.ac.id>
2023-05-11 11:47:14 +0700

    update functions

--- functions.php
+++ functions.php
@@ -52,6 +52,67 @@ function tambah($seller){
 }
 
 
+function tambahproduk($product){
+	global $conn;
+	$gambar = upload();
+	$nama = htmlspecialchars($product["nama"]); 
+	$harga = htmlspecialchars($product["harga"]);
+	$kadaluarsa = htmlspecialchars($product["kadaluarsa"]); 
+
+	if( !$gambar ){
+		return false;
+	}
+
+	$query = "INSERT INTO product 
+	VALUES ('', '$nama', '$gambar', '$harga', '$kadaluarsa', '1')";
+	mysqli_query($conn, $query);
+
+	return mysqli_affected_rows($conn);
+}
+
+
+function upload(){
+	$namaFile = $_FILES['gambar']['name'];
+	$ukuranFile = $_FILES['gambar']['size'];
+	$error = $_FILES['gambar']['error'];
+	$tmpName = $_FILES['gambar']['tmp_name'];
+
+	// cek gambar tidak diupload
+	if($error === 4){
+		echo "<script>
+			alert('Gambar belum dipilih');
+			</script>";
+		return false;
+	}
+
+	// cek ekstensi gambar
+	$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+	$ekstensiGambar = explode('.', $namaFile);
+	$ekstensiGambar = strtolower(end($ekstensiGambar));
+	if(!in_array($ekstensiGambar, $ekstensiGambarValid)){
+		echo "<script>
+			alert('File bukan gambar');
+			</script>";
+		return false;
+	}
+
+	// cek ukuran
+	if($ukuranFile > 10000000){
+		echo "<script>
+			alert('Ukuran gambar terlalu besar');
+			</script>";
+		return false;
+	}
+
+	// Generate nama gambar
+	$namaFileBaru = uniqid();
+	$namaFileBaru .= '.';
+	$namaFileBaru .= $ekstensiGambar;
+
+	// upload gambar
+	move_uploaded_file($tmpName, 'images/' . $namaFileBaru);
+	return $namaFileBaru;
+}
 
 
 function hapus($id) {
@@ -66,29 +127,19 @@ function hapus($id) {
 
 
 
-function edit($data) {
+function edit($product) {
 	global $conn;
-	$id = $data["id"];
-	$tanggal = htmlspecialchars($data["tanggal"]);
-	$st = htmlspecialchars($data["st"]);
-	$nama = htmlspecialchars($data["nama"]);
-	$sppd = htmlspecialchars($data["sppd"]); 
-	$awal = htmlspecialchars($data["awal"]);
-	$akhir = htmlspecialchars($data["akhir"]);
-	$sumber = htmlspecialchars($data["sumber"]);
-	$biaya = htmlspecialchars($data["biaya"]);
-	$ket = htmlspecialchars($data["ket"]);
-
-	$query = "UPDATE data SET 
-	tanggal = '$tanggal',
-	st = '$st',
+	$id = $product["id"];
+	$gambar = htmlspecialchars($product["gambar"]);
+	$nama = htmlspecialchars($product["nama"]);
+	$harga = htmlspecialchars($product["harga"]);
+	$kadaluarsa = htmlspecialchars($product["kadaluarsa"]); 
+	
+	$query = "UPDATE product SET 
+	gambar = '$gambar',
 	nama = '$nama',
-	sppd = '$sppd',
-	awal = '$awal',
-	akhir = '$akhir',
-	sumber = '$sumber',
-	biaya = '$biaya',
-	ket = '$ket'
+	harga = '$harga',
+	kadaluarsa = '$kadaluarsa'
 	WHERE id = '$id'";
 	mysqli_query($conn, $query);
 
