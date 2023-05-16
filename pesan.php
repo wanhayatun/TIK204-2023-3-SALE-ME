f124cd2812c3c5688ff24daa24d1463933d311e5
Wan Hayatun Nisa <wan_hayatu@mhs.unsyiah.ac.id>
2023-05-16 15:27:28 +0700

    adjust pesan produk

---
+++ pesan.php
@@ -0,0 +1,191 @@
+<?php 
+session_start();
+if (!isset($_SESSION["login"])) {
+	header("Location: loginpembeli.php");
+	exit;
+}
+require 'functions.php';
+
+if (isset($_POST["submit"])) {
+
+	if(pesan($_POST) > 0 ) {
+		echo "
+		<script>
+			alert('Pesanan Berhasil');
+			document.location.href = 'thankyou.php';
+		</script>
+		";
+	} 
+  //   else {
+	// 	echo "
+	// 	<script>
+	// 		alert('Pesanan Gagal');
+	// 		document.location.href = 'index.php';
+	// 	</script>
+	// 	";
+	// }
+}
+
+// $perhalaman = 10;
+// $jumlah = count(query("SELECT * FROM product"));
+// $jumlahhalaman = ceil($jumlah / $perhalaman);
+// // // if (isset($_GET["page"])) {
+// // // $halaktif = $_GET["page"];
+// // // } else {
+// // // 	$halaktif = 1;
+// // // }
+// $halaktif = (isset($_GET["page"])) ? $_GET["page"] : 1;
+// $awaldata = ($perhalaman * $halaktif) - $perhalaman;
+
+$id = $_GET["id"];
+$product = query("SELECT * FROM product WHERE id = $id")[0];
+
+if (isset($_POST["search"])) {
+	$data = search($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Sale Me &mdash; E-Commerce Food Waste</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <header class="site-navbar" role="banner">
+      <div class="site-navbar-top">
+        <div class="container">
+          <div class="row align-items-center">
+
+            <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+              <form action="" method="POST" class="site-block-top-search">
+                <span class="icon icon-search2"></span>
+                <input type="text" name="keyword" class="form-control border-0" placeholder="Search">
+              </form>
+              <!-- <form action="" method="POST" class="search">
+                <input type="text" name="keyword" size="25" placeholder="Cari Tanggal, Nama, No SPPD" autocomplete="off">
+                <button type="submit" name="search">&#x1F50D</button>
+              </form> -->
+            </div>
+
+            <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
+              <div class="site-logo">
+                <a href="index.php" class="js-logo-clone"><strong>Sale Me</strong></a>
+              </div>
+            </div>
+
+            <div class="col-6 col-md-4 order-3 order-md-3 text-right">
+              <div class="site-top-icons">
+                <ul>
+                  <li><a href="loginpembeli.php"><span class="icon icon-person"></span></a></li>
+                  <li><a href="logout.php" class="btn btn-primary text-white">logout</a></li>
+                  <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a></li>
+                </ul>
+              </div> 
+            </div>
+
+          </div>
+        </div>
+      </div> 
+      <nav class="site-navigation text-right text-md-center" role="navigation">
+        <div class="container">
+          <ul class="site-menu js-clone-nav d-none d-md-block">
+            <li class="active">
+              <a href="index.php">Home</a>
+            </li>
+            <li>
+              <a href="about.php">About</a>
+            </li>
+            <!-- <li><a href="shop.php">Shop</a></li> -->
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <div class="bg-light py-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Checkout</strong></div>
+        </div>
+      </div>
+    </div>
+
+  
+  <div class="site-wrap">
+    <header class="site-navbar" role="banner">
+      <div class="site-navbar-top">
+      <div class="container pt-4">
+        <h2>Order</h2>
+
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="" class="form-label">Nama Produk</label>
+                <input class="form-control" type="text" readonly class="form-control-plaintext" name="nama_produk" id="nama_produk" value="<?= $product["nama"] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Harga Produk</label>
+                <input class="form-control" type="number" readonly class="form-control-plaintext" name="harga_produk" id="harga_produk" value="<?= $product["harga"] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Nama</label>
+                <input class="form-control" type="text" name="nama" id="nama" required>
+            </div>
+            <div class="mb-3">
+                <label for="">Alamat</label>
+                <input class="form-control" type="text" id="alamat" name="alamat" required>
+            </div>
+            <div class="mb-3">
+                <label for="">No. Telepon</label>
+                <input class="form-control" type="text" id="telepon" name="telepon" required>
+            </div>
+            <div class="mb-3">
+                <label for="">Note</label>
+                <input class="form-control" type="text" id="note" name="note">
+            </div>
+            
+            <button type="submit" name="submit" class="btn btn-primary">Save</button>
+        </form>
+    </div>
+      </div>  
+
+    <footer class="site-footer border-top">
+      <div class="container">
+        <div class="row pt-1 mt-1 text-center">
+          <div class="col-md-12">
+            <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart" aria-hidden="true"></i> by <a href="index.php" target="_blank" class="text-primary">Sale Me Team</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
